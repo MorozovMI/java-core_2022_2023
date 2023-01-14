34f708c653ccc2898d9693becf7c83d5be26d5df
MorozovMI <misha.morozov0202@bk.ru>
2023-01-14 18:36:46 +0500

    initial commit

--- src/timus/task1639/Main.java
+++ src/timus/task1639/Main.java
@@ -2,7 +2,6 @@ package timus.task1639;
 
 import java.io.PrintWriter;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner in  = new Scanner(System.in);
@@ -15,4 +14,5 @@ public class Main {
         out.println((result!=0) ? ("[second]=:]") : ("[:=[first]"));
         out.flush();
     }
+
 }
