a93504574cf74184661692df40abbf1b29113586
MorozovMI <37338724+MorozovMI@users.noreply.github.com>
2023-05-07 03:42:19 +0500

    Delete src/testing (lr15) directory

--- src/testing (lr15)/src/test/java/ru/example/testing/service/ModifyRequestService.java
+++
@@ -1,6 +0,0 @@
-package ru.example.testing.service;
-
-import ru.example.testing.model.Request;
-public interface ModifyRequestService {
-    void modifyRq (Request request);
-}
--- src/testing (lr15)/src/test/java/ru/example/testing/service/MyModifyService.java
+++
@@ -1,7 +0,0 @@
-package ru.example.testing.service;
-import ru.example.testing.model.Response;
-public interface MyModifyService {
-    Response modify (Response response);
-}
-
-
